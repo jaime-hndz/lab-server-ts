08b24fca4f86fb9bba44018c04d4e193f1e354e1
Jaime <jaime251102@gmail.com>
2025-02-01 15:04:31 -0400

    modify authcontroller and add logger

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { User } from "../models/User";
+import { Logger } from "../decorators/Logger";
+
+
+export class AuthController {
+  @Logger
+  static async register(req: express.Request, res: express.Response) {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = new User({
+      username: req.body.username,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: "Usuario registrado" });
+  }
+
+  @Logger
+  static async login(req: express.Request, res: express.Response) {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      res.status(400).json({ message: "Usuario no encontrado" });
+    } else {
+      const isMatch = await bcrypt.compare(req.body.password, user.password);
+      if (!isMatch) {
+        res.status(400).json({ message: "Contraseña incorrecta" });
+      } else {
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET!, {
+          expiresIn: "1h",
+        });
+        res.json({ token });
+      }
+    }
+  }
+}
+
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,42 +1,10 @@
 import express from "express";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import { User } from "../models/User";
-import { Logger } from "../decorators/Logger";
+import { AuthController } from "../controllers/AuthController";
 
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const newUser = new User({
-      username: req.body.username,
-      password: hashedPassword,
-    });
+router.post("/register", AuthController.register);
+router.post("/login", AuthController.login);
 
-    await newUser.save();
-    res.status(201).json({ message: "Usuario registrado" });
-  } catch (error:unknown) {
-    const errorMessage = error instanceof Error ? error.message : String(error);
-    res.status(500).json({ error: errorMessage });
-  }
-});
-
-router.post("/login", async (req, res) => {
-  const user = await User.findOne({ username: req.body.username });
-  if (!user) {
-    res.status(400).json({ message: "Usuario no encontrado" });
-  } else {
-    const isMatch = await bcrypt.compare(req.body.password, user.password);
-    if (!isMatch) {
-      res.status(400).json({ message: "Contraseña incorrecta" });
-    } else {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET!, {
-        expiresIn: "1h",
-      });
-      res.json({ token });
-    }
-  }
-});
 
 export { router as authRouter };
